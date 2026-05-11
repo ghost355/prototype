9dd0b46cf8c7db28c8e62eacc0dcf3e8fd739ef8
Pavel Pavlov <kx355@mail.ru>
2026-05-11 21:52:35 +0300

    feat: add row columns maxBordersValues for Map

--- Sources/prototype/Core/GameState+Structs/Map.swift
+++ Sources/prototype/Core/GameState+Structs/Map.swift
@@ -2,4 +2,19 @@
 
 struct Map: Codable {
     let cells: [GridCoordinate: Cell]
+    let rows: Int
+    let columns: Int
+
+    let minColumn: Int
+    let maxColumn: Int
+    let maxRow: Int
+
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        self.minColumn = 0
+        self.maxColumn = columns - 1
+        self.maxRow = rows
+        self.cells = [:]
+    }
 }
